0d6e33f37344ad6c0f5956e009e508a7021f79b4
jkerpe <80028988+jkerpe@users.noreply.github.com>
2022-11-26 13:49:32 +0100

    Add files via upload
    added source file

---
+++ DeepSleepESP/DeepSleepESP.ino
@@ -0,0 +1,50 @@
+/*
+ * Code to wake up the ESP32 from Deepsleep over an Interrupt attached to an external Touchpin 
+ * 
+ * Created by: Maximilian Kühn, Juliane Kerpe
+ * Last modified on 03.11.2022
+ */
+#include <Wire.h>
+#define THRESHOLD 40
+
+
+// Function which gets called by interrupt
+void gotTouched(){
+
+}
+
+// Function to start the DeepSleep
+void startDeepSleep()
+{
+    esp_sleep_enable_touchpad_wakeup();
+    esp_deep_sleep_start();
+}
+
+// Other stuff can be made here, this is just a dummy
+void doSomethingElse(){
+  // LED an
+  Serial.println("Ich mache für 1 Sekunde was anderes...");
+  delay(1000);
+  // LED aus
+}
+
+// Caution: the setup function is called every time the ESP32 wakes up from deepsleep
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Ich bin wach");
+
+    // Attach the interrupt to an external Touchpin at T0 = GPIO4
+    touchAttachInterrupt(T0, gotTouched, THRESHOLD); 
+
+    doSomethingElse();
+    
+    Serial.println("Ich schlafe wieder");
+    Serial.println("");
+    startDeepSleep();
+
+}
+
+// the loop should never be called
+void loop() {
+  Serial.println("Schlecht!");
+}
